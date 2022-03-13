22989187ad0e73b164a6da1cfebc9ce25ed8953c
hosam8081 <hosamsalah8081@gmail.com>
2022-03-13 19:04:34 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    item:2,
+    loop:true,
+    margin:40,
+    nav:false,
+    autoplay:false,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
